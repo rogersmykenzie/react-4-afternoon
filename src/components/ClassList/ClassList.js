4e8d5adb16e1305af9ab1dc1d728dd13829adc0d
Mykenzie Rogers <mykenzierogers@Mykenzies-MacBook-Pro.local>
2018-12-11 14:33:42 -0600

    finished project

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3005/students?class=" + this.props.match.params.class)
+      .then(response => {
+        console.log(response);
+        this.setState({students: response.data});
+      })
   }
 
   render() {
+      let mapped = this.state.students.map((val, i, arr) => {
+        return <Link to={"/student/" + val.id} key={val.id}><h3>{val.first_name} {val.last_name}</h3></Link>
+      })
     return (
       <div className="box">
-        <h1></h1>
+        <Link to="/"><h1>Back</h1></Link>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mapped}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state = {
+      studentInfo: {}
+    }
+  }
 
+  componentDidMount() {
+    axios.get("http://localhost:3005/students/" + this.props.match.params.id)
+      .then(response => {
+        console.log(response);
+        this.setState({studentInfo: response.data});
+      })
+      console.log(this.props.cl);
   }
 
   render() {
     return (
       <div className="box">
+        <Link to={"/classList/" + this.props.match.params.id}><h1>Back</h1></Link>
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
       </div>
     )
   }
